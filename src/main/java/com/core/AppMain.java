847c8ebe351911b7de70a7a933c847268fb9cac2
dinhgiakhanh <khanhdg.195@gmail.com>
2024-07-09 16:48:52 +0700

    change

--- src/main/java/com/core/AppMain.java
+++ src/main/java/com/core/AppMain.java
@@ -25,7 +25,7 @@ public class AppMain {
     public static List<String> ipServerList;
 
     public static void main(String[] args) {
-        System.out.println(ApplicationName + " hello jenkins");
+        System.out.println(ApplicationName + " hello jenkins abcd");
         ConfigUtil.loadProperties();
         applicationStart(sourceLog);
 
